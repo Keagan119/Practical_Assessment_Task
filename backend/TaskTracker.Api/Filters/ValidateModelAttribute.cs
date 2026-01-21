0b27f7bd07f195655d476f541d901dbd5dded19c
keaganwilliams1198 <keaganwilliams1198@gmail.com>
2026-01-21 19:15:52 +0200

    Fix naming convention issue

--- backend/TaskTracker.Api/Filters/ValidateModelAttribute.cs
+++ backend/TaskTracker.Api/Filters/ValidateModelAttribute.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 namespace TaskTracker.Api.Filters
 {
-    public class ValidateModels : ActionFilterAttribute
+    public class ValidateModelAttribute : ActionFilterAttribute
     {
         public override void OnActionExecuting(ActionExecutingContext context)
         {
